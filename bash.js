2ae21131fffecc5a8f153bfb71c372493cf615ba
Nadine Ebrani <nadinedoshkeh3@gmail.com>
2020-09-25 16:25:48 -0400

    third commit

--- bash.js
+++ bash.js
@@ -17,7 +17,11 @@ process.stdin.on('data', (data) => {
         let fileName = cmd.slice(4)
         let catFunc = require('./cat');
         catFunc(fileName)
-    } 
+    } else if(cmd.slice(0, 4) === 'curl'){
+        let url = cmd.slice(5)
+        let curlFunc = require('./curl')
+        curlFunc(url)
+    }
     else {
        process.stdout.write('You typed: ' + cmd);
        process.stdout.write('\nprompt > ') 
